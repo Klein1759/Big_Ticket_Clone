8a4374c5bb01db2222e532d0659c61bb2303333f
Scott <Klein1759@users.noreply.github.com>
2018-05-17 17:47:35 -0400

    new commit

---
+++ src/big_ticket_clone/Big_Ticket_Clone.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package big_ticket_clone;
+
+/**
+ *
+ * @author Scott Klein
+ * This is going to be a basic clone of the Big Ticket System that I used at
+ * Boscovs when I worked there. This will not be too elaborate but after the
+ * basics are finished I will make it more elaborate.
+ */
+public class Big_Ticket_Clone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
